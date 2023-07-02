f95ffe5f91ce207776d9786650870d712ebd9747
Antony-Lester <me@antonylester.com>
2023-07-02 02:32:26 +0100

    empty project created

---
+++ src/tests/index.test.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line node/no-unpublished-import, node/no-extraneous-import
+//import {describe, expect, test} from '@jest/globals';
+//import {sum} from '../index';
+/*
+describe('function', () => {
+  test('then check', () => {
+    expect(function()).toBe('result');
+  });
+});
+*/
